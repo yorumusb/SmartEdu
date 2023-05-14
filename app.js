05f2b4b48d28f5ef3240753cb30155f318c6df8e
yorumusb <yorumusb@gmail.com>
2023-05-15 00:36:30 +0300

    template

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require("express");
 
 const app = express();
 
+// Template Engine
+app.set("view engine", "ejs");
+
+// Middlewares
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-    res.status(200).send("Index Page");
+    res.status(200).render("index", {
+        page_name: "index",
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.status(200).render("about", {
+        page_name: "about",
+    });
 });
 
 const port = 3000;
